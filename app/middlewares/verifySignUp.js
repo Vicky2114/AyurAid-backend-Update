5956fb5019b902d49bf6e8db8430aa0d00077c3c
shruti325 <sharmashruti71930@gmail.com>
2023-10-30 22:54:42 +0530

    fix

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -6,14 +6,18 @@ const checkDuplicateUsernameOrEmail = (req, res, next) => {
   User.findOne({ username: req.body.username })
     .then((user) => {
       if (user) {
-        res.status(400).send({ message: "Failed! Username is already in use!" });
+        res
+          .status(400)
+          .send({ message: "Failed! Username is already in use!" });
         return;
       }
 
       User.findOne({ email: req.body.email })
         .then((user) => {
           if (user) {
-            res.status(400).send({ message: "Failed! Email is already in use!" });
+            res
+              .status(400)
+              .send({ message: "Failed! Email is already in use!" });
             return;
           }
 
@@ -27,25 +31,9 @@ const checkDuplicateUsernameOrEmail = (req, res, next) => {
       res.status(500).send({ message: err });
     });
 };
-/* 
-const checkRolesExisted = (req, res, next) => {
-  if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.body.roles[i])) {
-        res.status(400).send({
-          message: `Failed! Role ${req.body.roles[i]} does not exist!`,
-        });
-        return;
-      }
-    }
-  }
-
-  next();
-}; */
 
 const verifySignUp = {
   checkDuplicateUsernameOrEmail,
-  // checkRolesExisted,
 };
 
 module.exports = verifySignUp;
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -12,5 +12,3 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
-
-
