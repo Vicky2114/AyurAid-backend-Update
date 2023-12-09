1cc2bdff5532067e25259dd3a6d5d54c143704ce
aditya142003 <adityabhatnagar142003@gmail.com>
2023-12-10 00:11:49 +0530

    Cors added

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
+const cors = require("cors");
 
 const app = express();
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 
 const PORT = process.env.PORT;
