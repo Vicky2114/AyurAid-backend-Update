293d445e66af6ce2f43879b2a8255507b35091f1
aditya142003 <adityabhatnagar142003@gmail.com>
2023-12-10 01:39:02 +0530

    exposedHeaders updated

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    exposedHeaders: ["set-cookie"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 
